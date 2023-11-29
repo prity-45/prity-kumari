522f4fcf44d898a12bee64bb9669ba6803bf2742
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-11-29 04:22:12 +0000

    file added

---
+++ 01-Basic/test.js
@@ -0,0 +1,10 @@
+console.log("hello.world")
+console.log("prity")
+console.log("singh")
+/*1. javascript is a high level scripting progamming language used on developing web& mobile application efficientiy
+2. we use javasript in both client side as well as server side laguage.
+3. we use javascript for developing logics in our code 
+4. java scrit was devloped in 1997
+5. now these days we used the ES6 version of javascript .it was strated on 2015 ans now it's the latest version of js
+ES6----Echma script 6(vanilla javascript)
+*/
