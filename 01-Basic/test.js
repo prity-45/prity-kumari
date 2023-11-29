6aa5e5df4147fc696f1446d160b9b18540f47d6a
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-11-29 05:01:34 +0000

    file added

--- 01-Basic/test.js
+++ 01-Basic/test.js
@@ -7,4 +7,26 @@ console.log("singh")
 4. java scrit was devloped in 1997
 5. now these days we used the ES6 version of javascript .it was strated on 2015 ans now it's the latest version of js
 ES6----Echma script 6(vanilla javascript)
+variable- its mutliple change in value.
+1. variable are container for storing data .
+2. javascript variable can be declared in 4 ways.
+i-automatically
+ii-using var
+iii-using let
+iv-using const.
+3. the var keyword was used is all javascript code from1995-2015
+4. the let and font keyword were added to javascript in 2015
+5. the var keyword is only we use in code return or older browers.
+6. let is blocked scope were as var is funtion scope.
 */
+const accountID = 334;
+let accountEmail = "pritykumari47481@gmail.com";
+var accountpassword ="12345";
+accountcity = "bbsr";
+let accountState ="odisha";
+console.log( accountID )
+console.log(accountEmail)
+console.log(accountpassword)
+console.log(accountcity)
+console.log(accountState)
+console.table([accountEmail,accountID,accountState,accountpassword,accountcity])
