45987ec147b082078f43ba09fef5667a3237ef37
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-11-30 04:09:31 +0000

    file added

---
+++ 01-Basic/datatype.js
@@ -0,0 +1,18 @@
+/* 
+1. to be able to operate on variable,it is important to know something about the type of data is called datatypes.
+2. javascript has 7 types of data types.
+i. number-2power53
+ii.bigint- 123456789*83768673*n
+iii. string-"abcd"
+iv.boolean-true/false
+v.null-0[]
+vi.underfined- not defined
+vii.symbol-uniqueness
+*/
+let name="prity";
+let age=21;
+let ispresent= true;
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof ispresent);
+
