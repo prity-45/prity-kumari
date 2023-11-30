879d86d6f2827f01f19ab600e66dfa91e8cc4f01
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-11-30 05:22:02 +0000

    file added

---
+++ 01-Basic/variablesummary.js
@@ -0,0 +1,13 @@
+/* in our avascript there are 2 types of variable is present.
+1.primitive-string,number,bigiting,symbol,
+2. non-primitive/refermce-
+i.array
+2. object
+3.function 
+const cars=["bmw","audi"];
+let myobj={
+    name:"prity",
+    age:21,
+    city:"bbsr"
+}
+*/
