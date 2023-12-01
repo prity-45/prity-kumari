d8aa6f02854e1cbe6d21e32956c469bde3c2aea3
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-12-01 04:09:13 +0000

    file added

---
+++ 01-Basic/switchsatement.js
@@ -0,0 +1,20 @@
+let num=5;
+switch(num){
+    case 0:
+    console.log("no is 300");
+          break;
+          case1:
+          console.log("no is 1");
+          break;
+       case2:
+       console.log("no is 2");
+          break;
+          case3:
+       console.log("no is 3");
+          break;
+          case4:
+       console.log("no is 4");
+          break;
+          default:
+            console.log("no is not matched");
+}
