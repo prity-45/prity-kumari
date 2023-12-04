dedc70509769d32ed33c29a1f2b402d7b764cffc
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-12-04 05:21:25 +0000

    file added

--- 01-Basic/numbermMath.js
+++ 01-Basic/numbermMath.js
@@ -13,3 +13,8 @@ console.log(Math.abs(-4));
 console.log(Math.round(4.6));
 console.log(Math.ceil(4.2)); //ceil is high.value
 console.log(Math.floor(4.9));// floor is lower.value
+console.log(Math.random());
+console.log(Math.random());
+const min =10;
+const max=20;
+console.log((Math.floor(Math.random()*(max-min+1))+min))
