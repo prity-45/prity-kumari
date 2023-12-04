3a43a43806874e01cf0b9ee540e8c20977fa2a6f
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-12-04 05:36:08 +0000

    file added

--- 01-Basic/dateAndtime.js
+++ 01-Basic/dateAndtime.js
@@ -3,3 +3,8 @@ console.log(mydate);
 console.log(mydate.toString());
 console.log(mydate.toDateString());
 console.log(mydate.toLocaleString);
+console.log(typeof mydate);
+let myownDate=new Date (2023,0,23,5,3)
+console.log(myownDate.toDateString());
+console.log(myownDate.toLocaleTimeString());
+
