ae12b07a47d498f16d27d870eb6d4396d95d9b2a
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-12-04 04:19:38 +0000

    file added

--- 01-Basic/string.js
+++ 01-Basic/string.js
@@ -6,3 +6,8 @@ console.log(`my name is ${name}`);        //  ` it is a backtics ESC down button
  constname= new String("prity");
  console.log(name);
  constname= new String("kumari");
+ console.log(name.length);
+ console.log(name.toUpperCase());
+ console.log(name.toLowerCase());
+ console.log(name.indexOf('n'));
+ console.log(name.charAt(2));
