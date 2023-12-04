7c6c379760e9aa67bc802adaa8d92fc8e853d093
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-12-04 05:15:16 +0000

    file aaded

--- 01-Basic/numbermMath.js
+++ 01-Basic/numbermMath.js
@@ -8,3 +8,8 @@ const otherNumber =125.8920;
 console.log(otherNumber.toPrecision(4));
 const demoNumber=1000000;
 console.log(demoNumber.toLocaleString());
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2)); //ceil is high.value
+console.log(Math.floor(4.9));// floor is lower.value
