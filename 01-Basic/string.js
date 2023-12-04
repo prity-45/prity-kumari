82f129cf61bce570fe3ae3c4c9f01be91e1b41e0
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-12-04 04:10:22 +0000

    file added

---
+++ 01-Basic/string.js
@@ -0,0 +1,8 @@
+const name ="prity";
+console.log(name);
+const count=50
+console.log(name+count)
+console.log(`my name is ${name}`);        //  ` it is a backtics ESC down button`
+ constname= new String("prity");
+ console.log(name);
+ constname= new String("kumari");
