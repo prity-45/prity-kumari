f339bc70a65d411f6b6c02448336f7f77097871e
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-12-05 04:08:20 +0000

    file added

---
+++ 02-intermediate/array.js
@@ -0,0 +1,8 @@
+//1. array is a object which can store similar type of data in a contigious memry location
+// 2. Array index start from 0
+const myarr =[3,4,5,6,7,8,9]  //array initiailze&declaring
+console.log(myarr[0])       //accesing the element
+const myArr =["giet","1","true"]
+console.log(myArr[1])
+const mynewArr =new Array(1,2,3,4)
+console.log(mynewArr);
