3cb0fdf2f0d6e782a4b18a732e9af2c4a5859309
prity-45 <152247293+prity-45@users.noreply.github.com>
2023-12-06 09:06:13 +0000

    file added

--- 02-intermediate/objectin.js
+++ 02-intermediate/objectin.js
@@ -4,16 +4,16 @@
 //BMW:"series7"
 //Audi:"Q7",
 //}
- const jsuser ={
-     name : "prity",
-     email : "pritykumari@gmail.com",
-     fullname:"prity singh",
-     age :"22",
-     "fullname":"prity singh",
-     location : "odisha",
-     isLogging:"false",
-     lastLoggedinDays : ["monday","wednesday","friday"]
- }
+const jsuser = {
+    name: "prity",
+    email: "pritykumari@gmail.com",
+    fullname: "prity singh",
+    age: "22",
+    "fullname": "prity singh",
+    location: "odisha",
+    isLogging: "false",
+    lastLoggedinDays: ["monday", "wednesday", "friday"]
+}
 //  console.log(jsuser.name);
 //  console.log(jsuser["email"]);
 //  console.log(jsuser["fullname"])
@@ -23,33 +23,62 @@
 //  console.log(jsuser.email);
 
 
-jsuser.greeting=function(){
+jsuser.greeting = function () {
 
     console.log("Hello future elon musk");
 }
 console.log(jsuser.greeting());
 
-jsuser.greetingTwo = function(){
+jsuser.greetingTwo = function () {
     console.log(`hello user, ${this.name}`);
 }
 console.log(jsuser.greeting());
 console.log(jsuser.greetingTwo());
 
-const tinderuser ={}
-tinderuser.id ="1234",
-tinderuser.email ="prity@gmail.com",
-tinderuser.name ="prity",
-console.log(tinderuser)
+const tinderuser = {}
+tinderuser.id = "1234",
+    tinderuser.email = "prity@gmail.com",
+    tinderuser.name = "prity",
+    console.log(tinderuser)
 
-const regularuser={
-    email:"prity@gmail.com",
-    fullname:{
-        userFullname:{
-            firstName:"prity",
-            lastName:"kumari"
+const regularuser = {
+    email: "prity@gmail.com",
+    fullname: {
+        userFullname: {
+            firstName: "prity",
+            lastName: "kumari"
         }
     }
 }
 console.log(regularuser.fullname.userFullname)
 
+const obj1 = {
+    1: 'a',
+    2: 'b'
+}
+const obj2 = {
+    3: 'a',
+    4: 'b'
+}
+const obj3 = {
+    5: 'a',
+    6: 'b'
+}
+const obj4 = { obj1, obj2, obj3 }
+console.log(obj4);
+const obj5 = { ...obj1, ...obj2, ...obj3 }
+console.log(obj5);
 
+
+//object destructuring
+const course = {
+    courseName: "last minute placement",
+    price: 9999,
+    courseInstructor: "prity",
+}
+const { courseInstructor: teacher } = course       //courseInstructor : teacher-de-structure the object.
+console.log(teacher);
+/* json-js object notation to help for storing data in our local disk and we can manipulatate these data also inone play to another
+  place.
+  API- application program interface -
+  it is a mechanism that enable two software component to communicate with each othr using a set of definationand protocal*/
